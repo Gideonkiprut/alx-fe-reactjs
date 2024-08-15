554bba15292798b4d523fd087e0483640916ca3f
Gideonkiprut <gideonkipruto695@gmail.com>
2024-08-15 10:52:32 -0700

    add welcome message

---
+++ alx-react-app/src/components/WelcomeMessage.jsx
@@ -0,0 +1,13 @@
+function App() {
+    return (
+      <div>
+        <Header />
+        <MainContent />
+        <Footer />
+        <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+      </div>
+    );
+  }
+  
+  export default App;
+  
