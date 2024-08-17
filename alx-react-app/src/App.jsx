358b3f1422a3da151a8934afca9419aba8f874ec
Gideonkiprut <gideonkipruto695@gmail.com>
2024-08-17 10:46:51 -0700

    uy

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import UserProfile from './UserProfile'; // Adjust the path if the file is in a different folder
+import Header from './components/Header';
+import UserProfile from './components/UserProfile';
+import Counter from './components/Counter';
 
 function App() {
   return (
     <div>
-      <h1>Welcome to My React App!</h1>
-      <UserProfile name="Gideon Kipruto" age={18} bio="I am a developer learning React." />
+      <Header />
+      <UserProfile name="Gideon Kipruto" age={25} bio="I am learning React." />
+      <Counter />
     </div>
   );
 }
